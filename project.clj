01377155784c465bbe46436d7f6187a3bb5ce5f3
Shane Mulligan <mullikine@gmail.com>
2022-01-23 12:20:13 +1300

    23.01.22

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [io.replikativ/datahike "0.4.0"]
+                 [io.replikativ/datahike-jdbc "0.1.2-SNAPSHOT"]]
   :main ^:skip-aot rhizome.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
